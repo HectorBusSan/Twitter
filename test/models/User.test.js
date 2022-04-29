09f33b6931991b1e222c1dc4639f0209e8263b4f
Hector <hector141599@gmail.com>
2022-04-29 06:28:25 -0500

    Agregando class Usur y prueba respectiva

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,5 +1,13 @@
-describe('Pruebas de jest',()=>{
-    test('Pruebas',()=>{
-        expect(10).toBe(9);
+const User = require('../../app/models/User');
+
+describe('Test to User',()=>{
+    test('Creación de Usuario',()=>{
+        const user= new User(1,"HéctorBusSan","Héctor","Bio","dateCreated","lastUpdate");
+        expect(user.id).toBe(1);
+        expect(user.username).toBe('HéctorBusSan');
+        expect(user.name).toBe('Héctor');
+        expect(user.bio).toBe('Bio');
+        expect(user.dateCreated).toBe('dateCreated');
+        expect(user.lasUpdate).toBe('lastUpdate');
     })
 })
